1e0d7a4f8beccbd32fe661b7548e626197c6398b
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-12-12 22:49:49 -0600

    finished the pixelate game

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // Your code here
+
+const table = document.getElementsByTagName('table')[0];
+const addRow = document.getElementById('add-row');
+const select = document.getElementsByTagName('select')[0]
+
+function makeRow() {
+    const tableRow = document.createElement('tr');
+    for (let i = 0; i < 20; i++) {
+        const tableData = document.createElement('td');
+        tableRow.appendChild(tableData);
+    }
+
+    table.appendChild(tableRow);
+
+};
+
+addRow.addEventListener('click', makeRow);
+
+function colorize(event) {
+    console.log("clicked");
+    console.log("this is event: ", event);
+    const target = event.target;
+    if (target.className === initialColor) {
+        target.className = ""
+    } else {
+        target.className = initialColor;
+    }
+}
+
+table.addEventListener('click', colorize);
+
+let initialColor = 'red'
+
+select.addEventListener('change', function(event) {
+    console.log(event.target.value);
+    initialColor = event.target.value;
+});
